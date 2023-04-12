17b8c7a39b10ccf6a77c44633a7edfeb50396ee9
Ciara Williams <125016758+CiaraWilliams1@users.noreply.github.com>
2023-04-12 11:04:42 +0100

    Python practice files
    analysing sales data for December for real estate company Lindstrom and Thomas - basic analysis

---
+++ C__Users_ciaraw_OneDrive - Sport England_Development_LSE data analytics_Course 2_my_first_code.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this is my first line of code
+
+print("My name is Ciara")
+print("The date is 12.04.2023")
+
+
+# In[2]:
+
+
+# Write a program to analyse real estate data
+# find the total sales for the month
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+print(a+b+c+d+e)
+
+
+# In[4]:
+
+
+# calculate the average sales price for the month 
+print((a+b+c+d+e)/5)
+
+
+# In[5]:
+
+
+# find the missing value from the last column of the table
+f = 67000-6500
+print(f)
+
+
+# In[ ]:
+
+
+
+
